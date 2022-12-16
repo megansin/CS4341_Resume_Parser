3af45b030e8ede1b76f36b206333e846460e6597
AllenCheung0213 <alcheung0213@gmail.com>
2022-12-15 20:25:54 -0500

    Adding the UI
    The home.html is the file to change how the website looks.

---
+++ api.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template,jsonify
+
+app = Flask(__name__)
+
+def do_something(jobDes,resume):
+   jobDes = jobDes
+   resume = resume
+   test1= 1
+   test2 =2
+   combine = test1 + test2
+   return combine
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/join', methods=['GET','POST'])
+def my_form_post():
+    jobDes = 1
+    resume = 2
+    combine = do_something(jobDes,resume)
+    result = {
+        "output": combine
+    }
+    result = {str(key): value for key, value in result.items()}
+    return jsonify(result=result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)

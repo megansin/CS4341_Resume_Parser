4b783baea05b78c332303f7bcabbaf51a1b9096b
megansin <megan_sin@student.wayland.k12.ma.us>
2022-12-18 20:45:25 -0500

    accounted for submissions with no file uploaded, no text pasted, and empty resume sections with None items

--- api.py
+++ api.py
@@ -1,6 +1,6 @@
 import os
 import pip
-from flask import Flask, request, render_template, jsonify, send_from_directory, abort
+from flask import Flask, request, render_template, flash, redirect
 from werkzeug.utils import secure_filename
 from KeywordFinder import get_keywords
 from ResumeParser import resume_parser
@@ -8,14 +8,15 @@ import difflib
 from fuzzywuzzy import fuzz
 
 app = Flask(__name__)
+app.secret_key = "something"
 app.config['UPLOAD_EXTENSIONS'] = ['.docx', '.pdf', '.txt']
 app.config['UPLOAD_PATH'] = '.'
 
 
 def intersect_keywords(resume, keywords):
-    skills = resume['skills']
-    degree = resume['degree']
-    experience = resume['experience']
+    skills = list(filter(lambda item: item is not None, resume['skills']))
+    degree = list(filter(lambda item: item is not None, resume['degree']))
+    experience = list(filter(lambda item: item is not None, resume['experience']))
     resume_vals = []
     for s in skills:
         resume_vals.extend(s.lower().split())
@@ -24,8 +25,10 @@ def intersect_keywords(resume, keywords):
     for e in experience:
         resume_vals.extend(e.lower().split())
 
-    found = set(keywords) & set(resume_vals)
+    found = set(keywords).intersection(set(resume_vals))
     missing = set(keywords) - found
+    found = list(found)
+    missing = list(missing)
 
     ratio = fuzz.token_set_ratio(keywords, resume_vals)
 
@@ -41,6 +44,10 @@ def home():
 def result():
     # if request.method == 'POST':
     job_text = request.form.get('text1')
+    if len(job_text) < 1:
+        flash('Empty job description, please enter text and try again', category='error')
+        return render_template("home.html")
+
     keywords = get_keywords(job_text)
 
     resume_file = request.files['filename']
@@ -48,11 +55,15 @@ def result():
     if filename != '':
         file_ext = os.path.splitext(filename)[1]
         if file_ext not in app.config['UPLOAD_EXTENSIONS']:
-            abort(400)
+            flash('Must be a .docx, .txt, or .pdf file', category='error')
+            return render_template("home.html")
         path = os.path.join(app.config['UPLOAD_PATH'], filename)
         resume_file.save(path)
         parsed_resume = resume_parser(path)
         missing, found, ratio = intersect_keywords(parsed_resume, keywords)
+    else:
+        flash('No file uploaded', category='error')
+        return render_template("home.html")
 
     return render_template('result.html', keywords=keywords, parsed_resume=parsed_resume, missing=missing, found=found,
                            ratio=ratio)
